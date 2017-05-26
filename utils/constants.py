a75c5b220bda5c04a436dfa64c21add8f717355d
Somshubra Majumdar <titu1994@gmail.com>
2017-05-26 12:06:53 -0500

    Refactored directory structure

--- utils/constants.py
+++ utils/constants.py
@@ -1,39 +1,39 @@
 
-TRAIN_FILES = ['../data\\Adiac_train__4_true.csv', # 0
-               '../data\\ArrowHead_train__4_true.csv',  # 1
-               '../data\\Beef_train__4_true.csv',  # 2
-               '../data\\BeetleFly_train__4_true.csv',  # 3
-               '../data\\BirdChicken_train__4_true.csv',  # 4
-               '../data\\Car_train__4_true.csv',  # 5
-               '../data\\CBF_train__4_true.csv',  # 6
-               '../data\\Coffee_train__4_true.csv',  # 7
-               '../data\\DiatomSizeReduction_train__4_true.csv',  # 8
-               '../data\\DistalPhalanxOutlineAgeGroup_train__4_true.csv',  # 9
-               '../data\\DistalPhalanxOutlineCorrect_train__4_true.csv',  # 10
-               '../data\\DistalPhalanxTW_train__4_true.csv',  # 11
-               '../data\\ECG200_train__4_true.csv',  # 12
-               '../data\\ECGFiveDays_train__4_true.csv',  # 13
-               '../data\\FaceFour_train__4_true.csv',  # 14
-               '../data\\FacesUCR_train__4_true.csv',  # 15
-               '../data\\ItalyPowerDemand_train__4_true.csv']  # 16
+TRAIN_FILES = ['./data\\Adiac_train__4_true.csv', # 0
+               './data\\ArrowHead_train__4_true.csv',  # 1
+               './data\\Beef_train__4_true.csv',  # 2
+               './data\\BeetleFly_train__4_true.csv',  # 3
+               './data\\BirdChicken_train__4_true.csv',  # 4
+               './data\\Car_train__4_true.csv',  # 5
+               './data\\CBF_train__4_true.csv',  # 6
+               './data\\Coffee_train__4_true.csv',  # 7
+               './data\\DiatomSizeReduction_train__4_true.csv',  # 8
+               './data\\DistalPhalanxOutlineAgeGroup_train__4_true.csv',  # 9
+               './data\\DistalPhalanxOutlineCorrect_train__4_true.csv',  # 10
+               './data\\DistalPhalanxTW_train__4_true.csv',  # 11
+               './data\\ECG200_train__4_true.csv',  # 12
+               './data\\ECGFiveDays_train__4_true.csv',  # 13
+               './data\\FaceFour_train__4_true.csv',  # 14
+               './data\\FacesUCR_train__4_true.csv',  # 15
+               './data\\ItalyPowerDemand_train__4_true.csv']  # 16
 
-TEST_FILES = ['../data\\Adiac_test__4_true.csv', # 0
-              '../data\\ArrowHead_test__4_true.csv', # 1
-              '../data\\Beef_test__4_true.csv', # 2
-              '../data\\BeetleFly_test__4_true.csv', # 3
-              '../data\\BirdChicken_test__4_true.csv', # 4
-              '../data\\Car_test__4_true.csv', # 5
-              '../data\\CBF_test__4_true.csv', # 6
-              '../data\\Coffee_test__4_true.csv', # 7
-              '../data\\DiatomSizeReduction_test__4_true.csv', # 8
-              '../data\\DistalPhalanxOutlineAgeGroup_test__4_true.csv', # 9
-              '../data\\DistalPhalanxOutlineCorrect_test__4_true.csv', # 10
-              '../data\\DistalPhalanxTW_test__4_true.csv', # 11
-              '../data\\ECG200_test__4_true.csv', # 12
-              '../data\\ECGFiveDays_test__4_true.csv', # 13
-              '../data\\FaceFour_test__4_true.csv', # 14
-              '../data\\FacesUCR_test__4_true.csv', # 15
-              '../data\\ItalyPowerDemand_test__4_true.csv'] # 16
+TEST_FILES = ['./data\\Adiac_test__4_true.csv', # 0
+              './data\\ArrowHead_test__4_true.csv', # 1
+              './data\\Beef_test__4_true.csv', # 2
+              './data\\BeetleFly_test__4_true.csv', # 3
+              './data\\BirdChicken_test__4_true.csv', # 4
+              './data\\Car_test__4_true.csv', # 5
+              './data\\CBF_test__4_true.csv', # 6
+              './data\\Coffee_test__4_true.csv', # 7
+              './data\\DiatomSizeReduction_test__4_true.csv', # 8
+              './data\\DistalPhalanxOutlineAgeGroup_test__4_true.csv', # 9
+              './data\\DistalPhalanxOutlineCorrect_test__4_true.csv', # 10
+              './data\\DistalPhalanxTW_test__4_true.csv', # 11
+              './data\\ECG200_test__4_true.csv', # 12
+              './data\\ECGFiveDays_test__4_true.csv', # 13
+              './data\\FaceFour_test__4_true.csv', # 14
+              './data\\FacesUCR_test__4_true.csv', # 15
+              './data\\ItalyPowerDemand_test__4_true.csv'] # 16
 
 MAX_NB_WORDS_LIST = [4095, 4095, 4095, 4093, 4093, 4095, 4095, 4095, 4095, 4095, 4095, 4095, 4093, 4095, 4095, 4095, 4095]
 
--- utils/keras_utils.py
+++ utils/keras_utils.py
@@ -12,7 +12,7 @@ from utils.generic_utils import load_dataset_at
 
 
 def train_model(model:Model, dataset_id, dataset_prefix, epochs=50, batch_size=128, test_data_subset=None):
-    logs_dir = "../logs/%s/" % (dataset_prefix)
+    logs_dir = "./logs/%s/" % (dataset_prefix)
 
     if not os.path.exists(logs_dir):
         os.makedirs(logs_dir)
@@ -31,7 +31,7 @@ def train_model(model:Model, dataset_id, dataset_prefix, epochs=50, batch_size=1
     y_train = to_categorical(y_train, len(np.unique(y_train)))
     y_test = to_categorical(y_test, len(np.unique(y_test)))
 
-    model_checkpoint = ModelCheckpoint("../weights/%s_weights.h5" % dataset_prefix, verbose=1,
+    model_checkpoint = ModelCheckpoint("./weights/%s_weights.h5" % dataset_prefix, verbose=1,
                                        monitor='val_acc', save_best_only=True, save_weights_only=True)
     tensorboard = TensorBoard(logs_dir, embeddings_freq=5, histogram_freq=5)
     reduce_lr = ReduceLROnPlateau(monitor='val_acc', patience=5, mode='max',
@@ -46,9 +46,9 @@ def train_model(model:Model, dataset_id, dataset_prefix, epochs=50, batch_size=1
         y_test = y_test[:test_data_subset]
 
     model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=callback_list,
-              class_weight=class_weight, verbose=2, validation_data=(X_test, y_test))
+              class_weight=class_weight, verbose=1, validation_data=(X_test, y_test))
 
-    model.load_weights("../weights/%s_weights.h5" % dataset_prefix)
+    model.load_weights("./weights/%s_weights.h5" % dataset_prefix)
 
     print("\nEvaluating : ")
     scores = model.evaluate(X_test, y_test, batch_size=batch_size)
