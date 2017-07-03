a72c496e89973e7cf1b7e8df8cb3e7fc1eb988ca
Somshubra Majumdar <titu1994@gmail.com>
2017-07-03 13:19:08 -0500

    Finished chlorine with 88.33%

--- chlorine_concentration_model.py
+++ chlorine_concentration_model.py
@@ -1,26 +1,27 @@
 from keras.models import Model
 from keras.layers import Input, PReLU, Dense,Dropout, LSTM, Bidirectional, multiply, concatenate
-from phased_lstm_keras.PhasedLSTM import PhasedLSTM
+from keras import backend as K
 
 from utils.constants import MAX_NB_WORDS_LIST, MAX_SEQUENCE_LENGTH_LIST, NB_CLASSES_LIST
-from utils.keras_utils import train_model, evaluate_model, set_trainable, visualise_attention
+from utils.keras_utils import train_model, evaluate_model, visualise_attention,set_trainable
 
 DATASET_INDEX = 2
-OUTPUT_DIM = 1000
-TRAINABLE = True
 
 MAX_SEQUENCE_LENGTH = MAX_SEQUENCE_LENGTH_LIST[DATASET_INDEX]
 MAX_NB_WORDS = MAX_NB_WORDS_LIST[DATASET_INDEX]
 NB_CLASS = NB_CLASSES_LIST[DATASET_INDEX]
 
+ATTENTION_CONCAT_AXIS = 1 # 1 = temporal, -1 = spatial
+TRAINABLE = True
+
 def generate_model():
 
-    ip = Input(shape=(1, MAX_SEQUENCE_LENGTH))
+    ip = Input(shape=(1, MAX_SEQUENCE_LENGTH,))
 
-    x = attention_block(ip)
-    x = concatenate([ip, x], axis=-1)
+    a = attention_block(ip, id=1)
+    x = concatenate([ip, a], axis=ATTENTION_CONCAT_AXIS)
 
-    x = Bidirectional(LSTM(128, trainable=TRAINABLE))(x)
+    x = Bidirectional(LSTM(512, trainable=TRAINABLE))(x)
     #x = PhasedLSTM(512)(x)
 
     x = Dense(1024, activation='linear')(x)
@@ -40,30 +41,29 @@ def generate_model():
 
     model.summary()
 
+    model.load_weights('weights/chlorine_concentration_weights - 512 8833 temporal.h5')
+
     return model
 
 
-def attention_block(inputs):
+def attention_block(inputs, id):
     # input shape: (batch_size, time_step, input_dim)
     # input shape: (batch_size, max_sequence_length, lstm_output_dim)
-    x = Dense(MAX_SEQUENCE_LENGTH, activation='softmax', name='attention_dense')(inputs)
+    input_dim = K.int_shape(inputs)[-1]
+    x = Dense(input_dim, activation='softmax', name='attention_dense_%d' % id)(inputs)
     x = multiply([inputs, x])
-    x = Dense(MAX_SEQUENCE_LENGTH, activation='softmax', name='attention_dense')(inputs)
-    x = multiply([inputs, x])
-    # x = Dense(MAX_SEQUENCE_LENGTH, activation='softmax', name='attention_dense')(inputs)
-    # x = multiply([inputs, x])
-    # x = Dense(MAX_SEQUENCE_LENGTH, activation='softmax', name='attention_dense')(inputs)
-    # x = multiply([inputs, x])
     return x
 
 
-if __name__ == "__main__":
+if __name__ == "_ _main__":
     model = generate_model()
 
-    train_model(model, DATASET_INDEX, dataset_prefix='chlorine_concentration', epochs=350, batch_size=64,
+    train_model(model, DATASET_INDEX, dataset_prefix='chlorine_concentration', epochs=100, batch_size=32,
                 val_subset=3840)
 
-    evaluate_model(model, DATASET_INDEX, dataset_prefix='chlorine_concentration', batch_size=32,
+    evaluate_model(model, DATASET_INDEX, dataset_prefix='chlorine_concentration', batch_size=128,
                   test_data_subset=3840)
+
+    visualise_attention(model, DATASET_INDEX, dataset_prefix='chlorine_concentration', layer_name='attention_dense_1')
 
 
