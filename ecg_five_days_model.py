eb6aeb233c8a9cdc36d78f7e51f2f2075696bcdc
Somshubra Majumdar <titu1994@gmail.com>
2017-07-05 23:30:14 -0500

    Finished ecg five days, italy, somy aibo 1

--- ecg_five_days_model.py
+++ ecg_five_days_model.py
@@ -1,11 +1,11 @@
 from keras.models import Model
-from keras.layers import Input, PReLU, Dense, Dropout, LSTM, concatenate, multiply
+from keras.layers import Input, PReLU, Dense, Dropout, LSTM, concatenate, multiply, Activation
 from keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute
 
 from utils.constants import MAX_NB_WORDS_LIST, MAX_SEQUENCE_LENGTH_LIST, NB_CLASSES_LIST
-from utils.keras_utils import train_model, evaluate_model, set_trainable, visualise_attention
+from utils.keras_utils import train_model, evaluate_model, set_trainable
 
-DATASET_INDEX = 16
+DATASET_INDEX = 13
 
 MAX_SEQUENCE_LENGTH = MAX_SEQUENCE_LENGTH_LIST[DATASET_INDEX]
 MAX_NB_WORDS = MAX_NB_WORDS_LIST[DATASET_INDEX]
@@ -17,23 +17,21 @@ TRAINABLE = True
 def generate_model():
     ip = Input(shape=(1, MAX_SEQUENCE_LENGTH))
 
-    x = attention_block(ip, id=1)
-    x = concatenate([ip, x], axis=ATTENTION_CONCAT_AXIS)
-
-    x = LSTM(8)(x)
+    x = LSTM(8)(ip)
+    x = Dropout(0.8)(x)
 
     y = Permute((2, 1))(ip)
     y = Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
     y = BatchNormalization()(y)
-    y = PReLU()(y)
+    y = Activation('relu')(y)
 
     y = Conv1D(256, 5, padding='same', kernel_initializer='he_uniform')(y)
     y = BatchNormalization()(y)
-    y = PReLU()(y)
+    y = Activation('relu')(y)
 
     y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
     y = BatchNormalization()(y)
-    y = PReLU()(y)
+    y = Activation('relu')(y)
 
     y = GlobalAveragePooling1D()(y)
 
@@ -74,9 +72,6 @@ def attention_block(inputs, id):
 if __name__ == "__main__":
     model = generate_model()
 
-    #train_model(model, DATASET_INDEX, dataset_prefix='italy_power_demand', epochs=2000, batch_size=64)
+    #train_model(model, DATASET_INDEX, dataset_prefix='ecg_five_days', epochs=2000, batch_size=128)
 
-    evaluate_model(model, DATASET_INDEX, dataset_prefix='italy_power_demand', batch_size=128)
-
-    visualise_attention(model, DATASET_INDEX, dataset_prefix='italy_power_demand', layer_name='attention_dense_1',
-                        visualize_sequence=True)
+    evaluate_model(model, DATASET_INDEX, dataset_prefix='ecg_five_days', batch_size=128)
--- sony_aibo_model.py
+++ sony_aibo_model.py
@@ -1,49 +1,78 @@
 from keras.models import Model
-from keras.layers import Input, PReLU, Dense,Dropout, LSTM, Embedding, BatchNormalization
+from keras.layers import Input, PReLU, Dense, Dropout, LSTM, concatenate, multiply, Activation
+from keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute
 
 from utils.constants import MAX_NB_WORDS_LIST, MAX_SEQUENCE_LENGTH_LIST, NB_CLASSES_LIST
-from utils.keras_utils import train_model, evaluate_model
+from utils.keras_utils import train_model, evaluate_model, set_trainable
 
 DATASET_INDEX = 17
-OUTPUT_DIM = 1000
 
 MAX_SEQUENCE_LENGTH = MAX_SEQUENCE_LENGTH_LIST[DATASET_INDEX]
 MAX_NB_WORDS = MAX_NB_WORDS_LIST[DATASET_INDEX]
 NB_CLASS = NB_CLASSES_LIST[DATASET_INDEX]
 
+ATTENTION_CONCAT_AXIS = 1 # 1 = temporal, -1 = spatial
+TRAINABLE = True
+
 def generate_model():
+    ip = Input(shape=(1, MAX_SEQUENCE_LENGTH))
 
-    ip = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+    x = LSTM(8)(ip)
+    x = Dropout(0.8)(x)
 
-    embedding = Embedding(input_dim=MAX_NB_WORDS, output_dim=OUTPUT_DIM,
-                          mask_zero=True, input_length=MAX_SEQUENCE_LENGTH)(ip)
+    y = Permute((2, 1))(ip)
+    y = Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
 
-    x = LSTM(512, dropout=0.2, recurrent_dropout=0.2)(embedding)
+    y = Conv1D(256, 5, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
 
-    x = BatchNormalization()(x)
+    y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
 
-    x = Dense(1024, activation='linear')(x)
-    x = PReLU()(x)
+    y = GlobalAveragePooling1D()(y)
 
-    x = Dropout(0.2)(x)
-
-    x = Dense(1024, activation='linear')(x)
-    x = PReLU()(x)
-
-    x = Dropout(0.2)(x)
+    x = concatenate([x, y])
 
     out = Dense(NB_CLASS, activation='softmax')(x)
 
     model = Model(ip, out)
+
+    cnn_count = 0
+    for layer in model.layers:
+        if layer.__class__.__name__ in ['Conv1D',
+                                        'BatchNormalization',
+                                        'PReLU']:
+            if layer.__class__.__name__ == 'Conv1D':
+                cnn_count += 1
+
+            if cnn_count == 3:
+                break
+            else:
+                set_trainable(layer, TRAINABLE)
+
     model.summary()
 
+    # add load model code here to fine-tune
+    model.load_weights('weights/sony_aibo_weights.h5')
+
     return model
+
+
+def attention_block(inputs, id):
+    # input shape: (batch_size, time_step, input_dim)
+    # input shape: (batch_size, max_sequence_length, lstm_output_dim)
+    x = Dense(MAX_SEQUENCE_LENGTH, activation='softmax', name='attention_dense_%d' % id)(inputs)
+    x = multiply([inputs, x])
+    return x
+
 
 if __name__ == "__main__":
     model = generate_model()
 
-    #train_model(model, DATASET_INDEX, dataset_prefix='sony_aibo', epochs=50, batch_size=16,
-    #            val_subset=601)
+    #train_model(model, DATASET_INDEX, dataset_prefix='sony_aibo', epochs=2000, batch_size=64)
 
-    evaluate_model(model, DATASET_INDEX, dataset_prefix='sony_aibo', batch_size=128,
-                test_data_subset=601)
+    evaluate_model(model, DATASET_INDEX, dataset_prefix='sony_aibo', batch_size=128)
