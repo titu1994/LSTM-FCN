add40d680e15b029a820e6e34ac8b2d7077f05b2
Somshubra Majumdar <titu1994@gmail.com>
2017-07-18 01:11:29 -0500

    Added AttentionLSTM to the models and AttentionLSTM layer

--- mote_strain_model.py
+++ mote_strain_model.py
@@ -1,9 +1,10 @@
 from keras.models import Model
-from keras.layers import Input, PReLU, Dense, Dropout, LSTM, concatenate, multiply, Activation
+from keras.layers import Input, PReLU, Dense, Dropout, LSTM, concatenate, multiply, Activation, add
 from keras.layers import Conv1D, BatchNormalization, GlobalAveragePooling1D, Permute
 
 from utils.constants import MAX_SEQUENCE_LENGTH_LIST, NB_CLASSES_LIST
 from utils.keras_utils import train_model, evaluate_model, set_trainable, visualize_cam
+from utils.layers import AttentionLSTM
 
 DATASET_INDEX = 25
 
@@ -11,6 +12,8 @@ MAX_SEQUENCE_LENGTH = MAX_SEQUENCE_LENGTH_LIST[DATASET_INDEX]
 NB_CLASS = NB_CLASSES_LIST[DATASET_INDEX]
 
 TRAINABLE = True
+
+ATTENTION_CONCAT_AXIS = 1
 
 
 def generate_model():
@@ -60,11 +63,58 @@ def generate_model():
     return model
 
 
+def generate_model_2():
+    ip = Input(shape=(1, MAX_SEQUENCE_LENGTH))
+
+    x = AttentionLSTM(8)(ip)
+    x = Dropout(0.8)(x)
+
+    y = Permute((2, 1))(ip)
+    y = Conv1D(128, 8, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+
+    y = Conv1D(256, 5, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+
+    y = Conv1D(128, 3, padding='same', kernel_initializer='he_uniform')(y)
+    y = BatchNormalization()(y)
+    y = Activation('relu')(y)
+
+    y = GlobalAveragePooling1D()(y)
+
+    x = concatenate([x, y])
+
+    out = Dense(NB_CLASS, activation='softmax')(x)
+
+    model = Model(ip, out)
+
+    cnn_count = 0
+    for layer in model.layers:
+        if layer.__class__.__name__ in ['Conv1D',
+                                        'BatchNormalization',
+                                        'PReLU']:
+            if layer.__class__.__name__ == 'Conv1D':
+                cnn_count += 1
+
+            if cnn_count == 3:
+                break
+            else:
+                set_trainable(layer, TRAINABLE)
+
+    model.summary()
+
+    # add load model code here to fine-tune
+
+    return model
+
+
 if __name__ == "__main__":
     model = generate_model()
 
-    # train_model(model, DATASET_INDEX, dataset_prefix='mote_strain', epochs=2000, batch_size=64)
+    #train_model(model, DATASET_INDEX, dataset_prefix='mote_strain', epochs=2000, batch_size=128)
 
-    evaluate_model(model, DATASET_INDEX, dataset_prefix='mote_strain', batch_size=128)
+    #evaluate_model(model, DATASET_INDEX, dataset_prefix='mote_strain', batch_size=128)
 
-    visualize_cam(model, DATASET_INDEX, dataset_prefix='mote_strain', class_id=0)
+    #visualize_cam(model, DATASET_INDEX, dataset_prefix='mote_strain_v4', class_id=0)
