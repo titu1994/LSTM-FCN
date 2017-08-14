Correct normalization

utils/generic_utils.py
@@ -52,7 +52,9 @@ def load_dataset_at(index, normalize_timeseries=False, verbose=True) -> (np.arra
         X_train = X_train[:, np.newaxis, :]
         # scale the values
         if normalize_timeseries:
-            X_train = (X_train - X_train.mean(axis=0)) / (X_train.std(axis=0))
+            X_train_mean = X_train.mean()
+            X_train_std = X_train.std()
+            X_train = (X_train - X_train_mean) / (X_train_std + 1e-8)
 
     if verbose: print("Finished loading train dataset..")
 
@@ -93,7 +95,7 @@ def load_dataset_at(index, normalize_timeseries=False, verbose=True) -> (np.arra
         X_test = X_test[:, np.newaxis, :]
         # scale the values
         if normalize_timeseries:
-            X_test = (X_test - X_test.mean(axis=0)) / (X_test.std(axis=0))
+            X_test = (X_test - X_train_mean) / (X_train_std + 1e-8)
 
     if verbose:
         print("Finished loading test dataset..")